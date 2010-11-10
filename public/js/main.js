8172ae5a6b8acd19827c822feec7ecc572ae5889
Joshua Ogle <joshua@joshuaogle.com>
2010-11-09 22:06:37 -0600

    style updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-	
-<script type="text/javascript" src="http://use.typekit.com/vbr1nkr.js"></script>
 	$.getScript("http://disqus.com/forums/joshuaogle/embed.js");
 });
 
@@ -10,4 +8,23 @@ function twitterCallback(obj) {
 	var id = obj[0].user.id;
 	document.getElementById('twitter_status').innerHTML = obj[0].text;
 	document.getElementById('twitter_status_time').innerHTML = relative_time(obj[0].created_at);
+}
+
+
+function relative_time(time_value) {
+  var parsed_date = Date.parse(time_value);
+  var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+  var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+  var pluralize = function (singular, n) {
+    return '' + n + ' ' + singular + (n == 1 ? '' : 's');
+  };
+  if(delta < 60) {
+  return 'less than a minute ago';
+  } else if(delta < (45*60)) {
+  return 'about ' + pluralize("minute", parseInt(delta / 60)) + ' ago';
+  } else if(delta < (24*60*60)) {
+  return 'about ' + pluralize("hour", parseInt(delta / 3600)) + ' ago';
+  } else {
+  return 'about ' + pluralize("day", parseInt(delta / 86400)) + ' ago';
+  }
 }
